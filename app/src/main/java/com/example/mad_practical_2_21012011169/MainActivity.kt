873182c1d796b61eeb864a33b16ac212669da620
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2023-07-25 10:22:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_2_21012011169/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mad_practical_2_21012011169
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    var textView: TextView? = null
+    val TAG = "MainActivity"
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate() method is called")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showMessage("onStart() method is called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showMessage("OnResume() method is called")
+    }
+    fun showMessage(msg:String){
+        Log.i(TAG, "$msg")
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+    }
+}
